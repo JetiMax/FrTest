d4e7b552d9dc1ef0705ec45f1df83699b74a0fe8
Max <91092934+JetiMax@users.noreply.github.com>
2023-07-10 13:51:20 +0300

    Update day24.tests.js

--- __tests__/day24.tests.js
+++ __tests__/day24.tests.js
@@ -11,24 +11,24 @@ describe("Day24", () => {
     })
 });
 
-it("moments in time", function () {
-    return frisby
-        .get('http://worldtimeapi.org/api/ip')
-        .expect("status", 200)
-        .expect('jsonTypes', [{
-            "abbreviation": "MSK",
-            "client_ip": "188.242.254.148",
-            "datetime": Joi.string().required(),
-            "day_of_week": 1,
-            "dst": false,
-            "dst_from": null,
-            "dst_offset": 0,
-            "dst_until": null,
-            "raw_offset": 10800,
-            "timezone": "Europe/Moscow",
-            "utc_datetime": Joi.string().required(),
-            "utc_offset": "+03:00",
-        }] )
+// it("moments in time", function () {
+//     return frisby
+//         .get('http://worldtimeapi.org/api/ip')
+//         .expect("status", 200)
+//         .expect('jsonTypes', [{
+//             "abbreviation": "MSK",
+//             "client_ip": "188.242.254.148",
+//             "datetime": Joi.string().required(),
+//             "day_of_week": 1,
+//             "dst": false,
+//             "dst_from": null,
+//             "dst_offset": 0,
+//             "dst_until": null,
+//             "raw_offset": 10800,
+//             "timezone": "Europe/Moscow",
+//             "utc_datetime": Joi.string().required(),
+//             "utc_offset": "+03:00",
+//         }] )
         .then((result) => console.log(result.json))
 
 });
